69bb64667e060e58d4d1f5adf46eb1480c00606c
nanatty32 <61622082+nanatty32@users.noreply.github.com>
2020-09-16 12:12:09 +0100

    Add files via upload

---
+++ src/Helpers/FormatsRequests.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ 
+ */
+
+namespace nanatty32\HubtelMerchantAccount\Helpers;
+
+
+trait FormatsRequests
+{
+    public function toJson($object)
+    {
+        $json = array();
+        if(!is_object($object))
+        {
+            throw new InvalidArgumentException('toJson formats only objects');
+        }
+        foreach(get_object_vars($object) as $property => $value )
+        {
+            $json[$property] = $value;
+        }
+        return json_encode($json);
+    }
+
+    public function flatten($array)
+    {
+        $flattened = array();
+        if(!is_array($array))
+        {
+            throw new InvalidArgumentException('flatten works with arrays only!');
+        }
+        foreach ($array as $value) {
+            if(!is_array($value))
+            {
+                if(!is_null($value)) $flattened[] = $value;
+            }else{
+                $flattened = array_merge($flattened,$this->flatten($value));
+            }
+        }
+        return $flattened;
+    }
+}
+
---
+++ src/Helpers/SendsRequests.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace nanatty32\HubtelMerchantAccount\Helpers;
+
+use GuzzleHttp\Client;
+use nanatty32\HubtelMerchantAccount\MobileMoney\Refund\Request as RefundRequest;
+use nanatty32\HubtelMerchantAccount\MobileMoney\Receive\Request as ReceiveMobileMoneyRequest;
+use nanatty32\HubtelMerchantAccount\OnlineCheckout\Request as OnlineCheckoutRequest;
+use nanatty32\HubtelMerchantAccount\OnlineCheckout\Response as OnlineCheckoutResponse;
+
+class SendsRequests
+{
+    use FormatsRequests;
+
+    protected $http;
+
+    protected $config;
+
+    private $auth;
+
+    public function __construct($config)
+    {
+        $this->http = new Client(['base_uri' => 'https://api.hubtel.com']);
+        $this->config = $config;
+        $this->auth = [$this->config['api_key']['client_id'], $this->config['api_key']['client_secret']];
+    }
+
+    /**
+     * @param ReceiveMobileMoneyRequest $request
+     * @return mixed Actual response body from gateway
+     */
+    public function sendReceiveMobileMoneyRequest(ReceiveMobileMoneyRequest $request)
+    {
+        $response = $this->http->request('POST', "/v1/merchantaccount/merchants/{$this->config['account_number']}/receive/mobilemoney", [
+            'headers'=>[
+                'Content-type' => 'application/json'
+            ],
+            'body' => $this->toJson($request),
+            'auth' => $this->auth
+        ]);
+
+        $this->checkResponseStatus($response);
+
+        return $response->getBody();
+    }
+
+    /**
+     * @param OnlineCheckoutRequest $request
+     * @return OnlineCheckoutResponse
+     */
+    public function sendOnlineCheckoutRequest(OnlineCheckoutRequest $request)
+    {
+        if (!$request->business->name) {
+            $request->business->name = $this->config['business']['name'];
+        }
+
+        $response = $this->http->request('POST', "/v1/merchantaccount/onlinecheckout/invoice/create", [
+            'json' => json_decode(json_encode($request), true),
+            'auth' => $this->auth
+        ]);
+
+        $this->checkResponseStatus($response);
+
+        $invoiceResponse = json_decode((string)$response->getBody());
+
+        return $invoiceResponse->response_text;
+    }
+
+    /**
+     * @param $token
+     * @return mixed
+     */
+    public function sendCheckInvoiceStatusRequest($token)
+    {
+        $response = $this->http->request('GET', "/v1/merchantaccount/onlinecheckout/invoice/status/{$token}");
+
+        $this->checkResponseStatus($response);
+
+        return json_decode((string)$response->getBody());
+    }
+
+    /**
+     * @param RefundRequest $request
+     * @return mixed Actual gateway response
+     */
+    public function sendRefundMobileMoneyRequest(RefundRequest $request)
+    {
+        $response = $this->http->request('POST', "/v1/merchantaccount/merchants/{$this->config['account_number']}/transactions/refund", [
+            'headers'=>[
+                'Content-type' => 'application/json'
+            ],
+            'body' => $this->toJson($request),
+            'auth' => $this->auth
+        ]);
+        $this->checkResponseStatus($response);
+        return $response->getBody();
+    }
+
+    private function checkResponseStatus($response)
+    {
+        if ($response->getStatusCode() !== 200) {
+            throw new \Exception((string)$response->getBody());
+        }
+        return $this;
+    }
+}
